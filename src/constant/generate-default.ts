14f3615736c9c47bda357a43858b045d9a3f8b9d
ducnam4298 <ducnam428@gmail.com>
2021-10-20 16:06:26 +0700

    create generate default

--- src/constant/generate-default.ts
+++ src/constant/generate-default.ts
@@ -1,6 +1,6 @@
 const GenerateDefault = class {
   string = (length?: number) => {
-    return [...Array(length ?? 10)].map(i => (~~(Math.random() * 36)).toString(36)).join('');
+    return [...Array(length ?? 10)].map(() => (~~(Math.random() * 36)).toString(36)).join('');
   };
 };
 
