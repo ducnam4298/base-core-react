f3c16e20fb29800b39f56522c833d7767e40ca75
ducnam4298 <ducnam4298@gmail.com>
2022-04-04 18:45:26 +0700

    merge build docker

--- src/routes/options.ts
+++ src/routes/options.ts
@@ -5,70 +5,68 @@ import { Permission, Role } from 'models/role';
 import { Dispatch } from 'redux';
 import { redirect, Bag } from 'redux-first-router';
 import { ActionCreators as ContextAction } from 'store/context';
-import { goToHome } from './Actions';
 
 const options = {
   onBeforeChange: async (dispatch: Dispatch<any>, getState: any, bag: Bag) => {
-    // const {
-    //   location: { routesMap },
-    // } = getState();
+    const {
+      location: { routesMap },
+    } = getState();
 
-    // const { isAuthenticated, user, permissions } = getState().ContextState; // boolean
-    // const { type } = bag.action; // {type, ...} = bag;
-    // const required = (routesMap[type] && routesMap[type].required) || '';
-    // const parentRole = (routesMap[type] && routesMap[type].parentRole) || [];
-    // const roles = (routesMap[type] && routesMap[type].roles) || [];
+    const { isAuthenticated, user, permissions } = getState().ContextState; // boolean
+    const { type } = bag.action; // {type, ...} = bag;
+    const required = (routesMap[type] && routesMap[type].required) || '';
+    const parentRole = (routesMap[type] && routesMap[type].parentRole) || [];
+    const roles = (routesMap[type] && routesMap[type].roles) || [];
 
-    // if (isAuthenticated && !user) {
-    //   dispatch(ContextAction.GetDataUser());
-    // }
+    if (isAuthenticated && !user) {
+      dispatch(ContextAction.GetDataUser());
+    }
 
-    // let dataPer: Permission[] = [...permissions];
-    // if (isAuthenticated && !permissions?.length) {
-    //   const res = await client.get(`${Endpoint.USER_URL}/my-roles`);
-    //   if (res && res?.status === 200) {
-    //     res.data.forEach((elem: Role) => {
-    //       dataPer = unionBy(dataPer, elem.permissions, 'id');
-    //     });
-    //     dispatch(ContextAction.GetRolesUser(dataPer));
-    //   } else {
-    //     dispatch(ContextAction.SwitchAuthenticated(SwitchAuthenticated.LOGGEDOUT));
-    //   }
-    // }
+    let dataPer: Permission[] = [...permissions];
+    if (isAuthenticated && !permissions?.length) {
+      const res = await client.get(`${Endpoint.USER_URL}/my-roles`);
+      if (res && res?.status === 200) {
+        res.data.forEach((elem: Role) => {
+          dataPer = unionBy(dataPer, elem.permissions, 'id');
+        });
+        dispatch(ContextAction.GetRolesUser(dataPer));
+      } else {
+        dispatch(ContextAction.SwitchAuthenticated(SwitchAuthenticated.LOGGEDOUT));
+      }
+    }
 
-    // let isPassedRole: boolean;
-    // let subRoles = (dataPer || [])
-    //   .filter(elem => (elem?.parentPermissionName || elem?.title) === parentRole)
-    //   .map(role => role.title);
+    let isPassedRole: boolean;
+    let subRoles = (dataPer || [])
+      .filter(elem => (elem?.parentPermissionName || elem?.title) === parentRole)
+      .map(role => role.title);
 
-    // if (subRoles?.length) {
-    //   isPassedRole = (roles || []).every((value: any) => subRoles.includes(value));
-    //   if (!isPassedRole) {
-    //     const action = redirect({ type: 'Home' });
-    //     dispatch(action);
-    //   }
-    // } else {
-    //   dispatch(ContextAction.SwitchAuthenticated(SwitchAuthenticated.LOGGEDOUT));
-    // }
+    if (subRoles?.length) {
+      isPassedRole = (roles || []).every((value: any) => subRoles.includes(value));
+      if (!isPassedRole) {
+        const action = redirect({ type: 'Home' });
+        dispatch(action);
+      }
+    } else {
+      dispatch(ContextAction.SwitchAuthenticated(SwitchAuthenticated.LOGGEDOUT));
+    }
 
-    // if (
-    //   required.includes('auth') &&
-    //   !isAuthenticated &&
-    //   getState().location.type !== 'Activation'
-    // ) {
-    //   dispatch(redirect({ type: 'Loading' }));
-    //   const action = redirect({ type: 'Signin' });
-    //   setTimeout(() => dispatch(action), 2000);
-    // } else if (
-    //   isAuthenticated &&
-    //   !required.includes('auth') &&
-    //   routesMap[type] &&
-    //   getState().location.type !== 'Activation'
-    // ) {
-    //   const action = redirect({ type: 'Home' });
-    //   dispatch(action);
-    // }
-    goToHome();
+    if (
+      required.includes('auth') &&
+      !isAuthenticated &&
+      getState().location.type !== 'Activation'
+    ) {
+      dispatch(redirect({ type: 'Loading' }));
+      const action = redirect({ type: 'Signin' });
+      setTimeout(() => dispatch(action), 2000);
+    } else if (
+      isAuthenticated &&
+      !required.includes('auth') &&
+      routesMap[type] &&
+      getState().location.type !== 'Activation'
+    ) {
+      const action = redirect({ type: 'Home' });
+      dispatch(action);
+    }
   },
 };
 
